70d8c300bc0ea3201d0f97bd3e02fb9d662581ba
wwalice <wwalice@yahoo.com>
2022-07-02 11:29:33 -0400

    Initial comm it

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Index() {
+    return (
+        <>
+            <h1>Hi I am Jing Feng. I am happy to create my first webpage!</h1>
+            <p>This is a drawing I love</p>
+            <img src="/post.jpeg" alt="hh" />
+        </>
+    );
+}
